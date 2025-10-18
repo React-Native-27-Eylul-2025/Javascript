9d925e572bdd5f3293e7831fdec7f6cbc5b9273d
bedirhan-1 <bedirhan9828@gmail.com>
2025-10-18 21:38:24 +0300

    ornek 3 solution

--- ornekler/ornek3.js
+++ ornekler/ornek3.js
@@ -1,13 +1,23 @@
 // bir x değeri tanımlayın ve bu değerin tek mi 
 // çift mi olduğunu yazdıran programı yazın
 
-var x = 20
+// var x = 20
 
-if ( x % 2 == 0 ) {
-    console.log("çifttir")
-} else {
-    console.log("tektir")
-}
+// if ( x % 2 == 0 ) {
+//     console.log("çifttir")
+// } else {
+//     console.log("tektir")
+// }
 
 // eğer çift ise -> true
 // tek ise -> false
+
+function tekMi(x){
+    if ( x % 2 == 0 ) {
+        return false
+    } else {
+        return true
+    }
+}
+
+console.log(tekMi(13))
